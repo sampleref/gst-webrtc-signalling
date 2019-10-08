7b72631b0e397c2ccce565fba6d086b5c891ace2
Chakradhar Kantipudi <chakradhar.kantipudi@nokia.com>
2019-10-08 13:02:03 +0530

    Added openshift ports

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ from concurrent.futures._base import TimeoutError
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 # See: host, port in https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.create_server
-parser.add_argument('--addr', default=os.environ['OPENSHIFT_PYTHON_IP'], help='Address to listen on (default: all interfaces, both ipv4 and ipv6)')
-parser.add_argument('--port', default=int(os.environ['OPENSHIFT_PYTHON_PORT']), type=int, help='Port to listen on')
+parser.add_argument('--addr', default='', help='Address to listen on (default: all interfaces, both ipv4 and ipv6)')
+parser.add_argument('--port', default=8443, type=int, help='Port to listen on')
 parser.add_argument('--keepalive-timeout', dest='keepalive_timeout', default=30, type=int, help='Timeout for keepalive (in seconds)')
 parser.add_argument('--cert-path', default=os.path.dirname(__file__))
 parser.add_argument('--disable-ssl', default=False, help='Disable ssl', action='store_true')
